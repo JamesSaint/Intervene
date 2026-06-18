d35f90012659747830681ba53834576812617013
James Saint <james@jamessaint.com>
2026-06-18 10:07:46 +0100

    linkedin added

---
+++ src/lib/social.ts
@@ -0,0 +1,4 @@
+export const socialLinks = {
+  jamesSaintLinkedIn: 'https://www.linkedin.com/in/jamessaint/',
+  interveneLinkedIn: 'https://www.linkedin.com/company/intervenelimited/',
+} as const;
